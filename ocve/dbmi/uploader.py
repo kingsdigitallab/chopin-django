7b8655319ac72fa51791ff680ab4012a548fb4c9
ehall <ehall@localhost>
2015-05-15 17:42:12 +0100

    unicode upload

--- ocve/dbmi/uploader.py
+++ ocve/dbmi/uploader.py
@@ -52,7 +52,8 @@ def newsourcefiles(request):
 def convertFolder(request,folderName):
     newS=NewSource(label=folderName,sourcecreated=0)
     newS.save()
-    files=os.listdir(unicode(settings.CONVERTED_UPLOAD_PATH+u'/'+folderName,'utf8'))
+    folderName=folderName.decode('utf-8')
+    files=os.listdir(settings.CONVERTED_UPLOAD_PATH+u'/'+folderName)
     for f in files:
         npi = NewPageImage(source=newS, filename=f, surrogate=1, versionnumber=1, permission=False,
                         permissionnote='', height=0, width=0, startbar=0, endbar=0, corrected=0)
@@ -70,7 +71,7 @@ def convertFolder(request,folderName):
         except OSError:
                 logger.error(OSError)
         else:
-            logger.debug(unicode('Copied '+settings.CONVERTED_UPLOAD_PATH+'/'+'/'+folderName+'/'+f+'" "'+settings.NEWJP2_UPLOAD_PATH+'/'+str(npi.id)+'.jp2"','utf8'))
+            logger.debug('Copied '+settings.CONVERTED_UPLOAD_PATH+'/'+'/'+folderName+'/'+f+'" "'+settings.NEWJP2_UPLOAD_PATH+'/'+str(npi.id)+'.jp2"')
 
     return HttpResponseRedirect('/ocve/sourceeditor/new/' + str(newS.id))
 
