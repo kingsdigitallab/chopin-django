0f70249b8264c0136e0428db1051eef4cb6f7cb8
Ginestra <ginestra@users.noreply.github.com>
2015-06-01 10:11:41 +0100

    Merge from default

--- ocve/annotationviews.py
+++ ocve/annotationviews.py
@@ -1,47 +1,55 @@
-from django.template import RequestContext
-from ocve.forms import AnnotationForm
-from ocve.models import PageImage, Annotation
-from ocve.models_generic import AnnotationType
-
 __author__ = 'Elliot'
+
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 from django.shortcuts import render_to_response
-from django.http import HttpResponse, HttpResponseRedirect
-from django.template.context import RequestContext
+from django.template import RequestContext
+from django.template.loader import render_to_string
+from django.views.decorators.csrf import csrf_exempt
 
 from bartools import toGeos
-from ocve.models import Annotation_BarRegion,Bar,BarRegion
-from django.views.decorators.csrf import csrf_exempt
-from django.contrib.auth.decorators import login_required
+from ocve.forms import AnnotationForm
+from ocve.models import (Annotation, Annotation_BarRegion, Bar, BarRegion,
+                         PageImage)
+from ocve.models_generic import AnnotationType
 
 
-#Note edit views
+# Note edit views
 
-OP_INSERT = 1
-OP_UPDATE = 2
-OP_DELETE = 3
-
-#Mini wrapper class for geojson
+# Mini wrapper class for geojson
 class noteGeos:
-    def __init__(self, annotation):
-        self.annotation=annotation
-        self.barregions=annotation.getBarRegions()
-        if self.barregions.count() > 0:
-            self.geos=toGeos(self.barregions)
-        else:
-            self.geos=[]
 
-#Delete user annotation
+    def __init__(self, annotation):
+        self.annotation = annotation
+        self.barregions = annotation.getBarRegions()
+        self.geos = []
+
+        if self.barregions.count() > 0:
+            self.geos = toGeos(self.barregions)
+
+# Delete user annotation
 @csrf_exempt
 @login_required
-def deleteNote(request,id):
+def deleteNote(request, id):
     try:
         Annotation.objects.get(id=id).delete()
     except:
         pass
-    return HttpResponse("Deleted")
+
+    messages.info(request, 'Annotation deleted.')
+    rendered_messages = render_to_string(
+        'frontend/messages.html', {'messages': messages.get_messages(request)})
+
+    data = {
+        'messages': rendered_messages
+    }
+
+    return render_to_response('frontend/ajax/updatenote.html', data,
+                              RequestContext(request))
 
 
-#Takes an annotation form and updates
+# Takes an annotation form and updates
 @csrf_exempt
 @login_required
 def saveNote(request):
@@ -49,12 +57,12 @@ def saveNote(request):
     annotation = None
 
     if int(annotation_id) > 0:
-        #Update
+        # Update
         annotation = Annotation.objects.get(id=int(annotation_id))
-        #Clear previous notes
+        # Clear previous notes
         Annotation_BarRegion.objects.filter(annotation=annotation).delete()
     else:
-        #Insert Annotation()
+        # Insert Annotation()
         annotation = Annotation()
 
     try:
@@ -69,19 +77,20 @@ def saveNote(request):
 
     new_annotation = form.save()
 
-    #Transform POLYGON feature def for later GeoJSON export
-    #POLYGON((1426 2368,1170 2036,1358 1824,1350 2084,1526 2152,1426 2368))
+    # Transform POLYGON feature def for later GeoJSON export
+    # POLYGON((1426 2368,1170 2036,1358 1824,1350 2084,1526 2152,1426 2368))
     geotext = new_annotation.noteregions
-    if len(geotext) > 0 :
-        geotext=geotext.replace('POLYGON((', '').replace('))', '').replace(
+    if len(geotext) > 0:
+        geotext = geotext.replace('POLYGON((', '').replace('))', '').replace(
             ',', '],[').replace(' ', ',')
         new_annotation.noteregions = '[' + geotext + ']'
         new_annotation.save()
-    
-    #Recalculate which bar regions intersect with this note
+
+    # Recalculate which bar regions intersect with this note
     try:
         bars = request.POST['noteBars']
         labels = notebars.split(',')
+
         for label in labels:
             bar = Bar.objects.filter(barlabel=label)
             if bar and bar.count() > 0:
@@ -94,19 +103,35 @@ def saveNote(request):
     except Exception:
         pass
 
-    return render_to_response('frontend/ajax/updatenote.html',
-                              {'note': new_annotation})
+    messages.info(request, 'Annotation saved.')
+    rendered_messages = render_to_string(
+        'frontend/messages.html', {'messages': messages.get_messages(request)})
+
+    data = {
+        'note': new_annotation,
+        'messages': rendered_messages
+    }
+
+    return render_to_response('frontend/ajax/updatenote.html', data,
+                              RequestContext(request))
 
 
 @csrf_exempt
-def getAnnotations(request,id):
-    notes=Annotation.objects.filter(annotation_barregion__barregion_id=id,type_id__gt=1)
-    return render_to_response('frontend/ajax/annotations.html',{'notes': notes})
+def getAnnotations(request, id):
+    notes = Annotation.objects.filter(
+        annotation_barregion__barregion_id=id, type_id__gt=1)
+
+    return render_to_response('frontend/ajax/annotations.html',
+                              {'notes': notes})
+
 
 @csrf_exempt
-def getAnnotationRegions(request,id):
-    notes=Annotation.objects.filter(pageimage_id=id)
-    annotations=[]
+def getAnnotationRegions(request, id):
+    notes = Annotation.objects.filter(pageimage_id=id)
+    annotations = []
+
     for n in notes:
         annotations.append(noteGeos(n))
-    return render_to_response('geojson.html', {'annotations': annotations, 'grouped': 0})
+
+    return render_to_response('geojson.html',
+                              {'annotations': annotations, 'grouped': 0})
