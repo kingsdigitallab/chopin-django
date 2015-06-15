84c20832ec2e43843f21c9594f42f73699eb34cf
Alejandro Giacometti <alejandro.giacometti@gmail.com>
2015-06-15 18:35:32 +0100

    more extenralising browse javascript

--- ocve/static/javascripts/browse.js
+++ ocve/static/javascripts/browse.js
@@ -1,6 +1,6 @@
 // (function($, config){
 
-addSelectedFilter = function(type, selection) {
+var addSelectedFilter = function(type, selection) {
   var $selected_filters_for_type = $(selectedFilterSelector + '.' + type),
     $selected_filters = $('#selectedFilters'),
     $no_filters_el = $(noFiltersSelector),
@@ -22,7 +22,7 @@ addSelectedFilter = function(type, selection) {
 
 };
 
-applyFilter = function(type, id, selection) {
+var applyFilter = function(type, id, selection) {
   //Apply a pouroverfilter and update the selectedfilters display
   // type: filter type
   //Id: the numeric id in db
@@ -42,7 +42,7 @@ applyFilter = function(type, id, selection) {
   serializeFilters();
 };
 
-serializeFilters = function() {
+var serializeFilters = function() {
   // this function sends the current set of filters to
   // the server
   var current_filter_prefixed = config.mode + '_current_filters';
@@ -51,7 +51,7 @@ serializeFilters = function() {
   });
 };
 
-removeFromFilterArray = function(type) {
+var removeFromFilterArray = function(type) {
   // removes a filter from the filters array
   for (var f in filters) {
     if (filters[f].type == type) {
@@ -60,7 +60,7 @@ removeFromFilterArray = function(type) {
   }
 };
 
-removeSelectedFilter = function(type) {
+var removeSelectedFilter = function(type) {
   // This function gets called when a filter is added
   // or removed brom the browser
 
@@ -71,7 +71,7 @@ removeSelectedFilter = function(type) {
   serializeFilters();
 };
 
-filterFacets = function(filteredPages, exclude) {
+var filterFacets = function(filteredPages, exclude) {
   var filteredWorks = {},
     filteredGenres = {},
     filteredPublishers = {},
@@ -129,6 +129,190 @@ filterFacets = function(filteredPages, exclude) {
 };
 
 
+var filterSelections = function(selections, validSelections, selectionSelector) {
+  var $selectionSelector = $(selectionSelector);
+
+  for (var i in selections) {
+    if (!validSelections.hasOwnProperty(selections[i])) {
+      $selectionSelector.find('[data-key=' + selections[i] + ']').parent('li').hide();
+    } else {
+      $selectionSelector.find('[data-key=' + selections[i] + ']').parent('li').show();
+    }
+  }
+};
+
+
+var outputWorkGroup = function(sourceList, opusOutput, opusid) {
+  var output = ["<div id=\"opus", opusid, "\" class=\"opus\">",
+    "<h2> ", workLabels[opusid],
+    " <span class=\"contextual-info\">"
+  ];
+
+  if (workinfos.indexOf(opusid) > -1) {
+    output = output.concat(["<a href=\"#\" class=\"ctrl more\" data-tooltip=\"\" data-reveal-ajax=\"/",
+      config.mode.toLowerCase(),
+      "/browse/workinformation/",
+      opusid,
+      "/\" title=\"Work overview\" data-reveal-id=\"opus-info\">",
+      "<span class=\"fa fa-book\"></span>",
+      "</a>"
+    ]);
+  }
+  output = output.concat(["</span>", "</h2>", opusOutput, "</div>"]);
+  return output.join("");
+};
+
+var outputPages = function(source, availInstruments) {
+  //Output the determined number of preview pages for each source with further links
+  var output = [],
+    sourcePages = source.Pages,
+    scid = 0,
+    workid = 0;
+
+  for (var i = 0; i < filters.length; i++) {
+    if (filters[i].type == 'Work') {
+      workid = filters[i].id
+    }
+  }
+
+  for (var x = 0; x < sourcePages.length; x++) {
+    var scInstruments;
+    if (sourcePages[x].sourcecomponent_id != scid) {
+
+      scInstruments = sourcecomponentsById[sourcePages[x].sourcecomponent_id].instruments;
+
+      if (scInstruments) {
+        for (var i = 0; i < scInstruments.length; i++) {
+          availInstruments['' + scInstruments] = 1;
+        }
+      } else {
+        scInstruments = [];
+      }
+
+      output = output.concat([" <li data-instruments=\"", scInstruments.toString(), "\"",
+        " id=\"page", sourcePages[x].id, "\"",
+        " class=\"page flag\">",
+        "<p>", sourcecomponentsById[sourcePages[x].sourcecomponent_id].label, "</p>"
+      ]);
+
+      scid = sourcePages[x].sourcecomponent_id;
+
+    } else {
+      // TODO: scInstruments here is always empty, can we have a look
+      output = output.concat(["<li data-instruments=\"", scInstruments.toString(), "\"",
+        " id=\"page", sourcePages[x].id, "\" class=\"page\">"
+      ]);
+    }
+
+    var source_pages_label_display = (sourcePages[x].label.length > 30) ?
+      sourcePages[x].label.substring(0, 27) + "..." :
+      sourcePages[x].label;
+
+    output = output.concat(["<a class=\"pageView th\" data-sourcekey=\"", sourcePages[x].id, "\"",
+      " href=\"", pageviewURL, "/", sourcePages[x].id, "/\">",
+      "<img", " class=\"lazy\" data-original=\"/thumbnails/", sourcePages[x].id, ".jpg\" />",
+      "</a>",
+      "<span class=\"page-label\" title=\"", sourcePages[x].label, "\">",
+      source_pages_label_display,
+      "</span>",
+      "</li>"
+    ]);
+
+  }
+  return output.join("");
+};
+
+var outputSources = function(sources) {
+  var output = [],
+    opusOutput = [],
+    sourceList = [],
+    opusid = 0,
+    availInstruments = {
+      "0": 1
+    };
+
+  //Filter matching opus quickjump
+  for (var x = 0; x < sources.length; x++) {
+    if (opusid == 0) {
+      opusid = sources[x].Work;
+    }
+    if (sources[x].Work != opusid) {
+
+      output = output.concat(
+        outputWorkGroup(
+          sourceList.join(""),
+          opusOutput.join(""),
+          opusid));
+      opusid = sources[x].Work;
+      opusOutput = [];
+      sourceList = [];
+    }
+    //Write source in quick list for opus
+    sourceList = sourceList.concat(["<li><a href=\"#source", sources[x].id, "\" class=\"sourceLink\"",
+      " data-sourcekey=\"", sources[x].id, "\">",
+      sources[x].label, "</a>"
+    ]);
+
+    //Collect all the sources in the opus
+    opusOutput = opusOutput.concat(["<div id=\"source", sources[x].id, "\" class=\"source\">",
+      "<span class=\"contextual-info right\" >"
+    ]); //Edit link to source editor, for stage only.
+
+
+    if (sources[x].achash.length > 0) {
+      opusOutput = opusOutput.concat([
+        "<a class=\"ctrl more\" data-tooltip=\"\"",
+        " title=\"View in Annotated Catalogue\" href=\"/aco/catalogue/impression/",
+        sources[x].achash, "/\">",
+        "<i class=\"fa fa-link\"></i></a>&nbsp;"
+      ]);
+    }
+    opusOutput = opusOutput.concat([
+      "<a href=\"#\" class=\"ctrl more\" data-tooltip=\"\"",
+      " data-reveal-ajax=\"/", config.mode.toLowerCase(), "/browse/sourceinformation/", sources[x].id, "/\"",
+      " title=\"Witness overview\" data-reveal-id=\"witness-info\">",
+      "<i class=\"fa fa-info-circle fa-lg\"></i></a>",
+      "</span>",
+      "<h5><a href=\"#\" class=\"expandme\"><i class=\"expandd fa fa-caret-right\"></i> ", sources[x].label, "</a></h5>",
+      "<ul id=\"sourcePages", sources[x].id, "\" class=\"pageList movement\">",
+      outputPages(sources[x], availInstruments),
+      "</ul>", "</div>"
+    ]);
+
+  }
+
+  //Output last group
+  output = output.concat(
+    outputWorkGroup(sourceList.join(""),
+      opusOutput.join(""),
+      opusid)
+  );
+
+  //Show available instrument filters
+  showAvailableFilters(availInstruments);
+
+
+  return output.join("");
+}
+
+var showAvailableFilters = function(availInstruments) {
+  var avail = 0;
+  //Show Instrument filters
+  $('li.instrumentFilter').each(function(index) {
+    if (availInstruments['' + $(this).data('instrument_id') + ''] == 1 && $(this).data('instrument_id') != 2) {
+      avail += 1;
+      $(this).show();
+    } else if ($(this).data('instrument_id') != '0') {
+      $(this).hide();
+    }
+  });
+  if (avail > 2) {
+    $('#instrumentFilterControl').show();
+  } else {
+    $('#instrumentFilterControl').hide();
+    $('#instrumentToggle').html('Filter by instrument');
+  }
+};
 
 $(document).ready(function() {
   // setup events
