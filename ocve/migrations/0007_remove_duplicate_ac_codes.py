7ce9df0f2b379ae9ef60078fef66837c583650c1
jvieira <jmiguelv@users.noreply.github.com>
2015-06-17 11:35:49 +0100

    Changed the AcCode model to make the AC codes unique. Added migration to invalidate the AC codes that are potential duplicates, ie, they are not connected to any Sources.

---
+++ ocve/migrations/0007_remove_duplicate_ac_codes.py
@@ -0,0 +1,37 @@
+from django.db import migrations
+from django.db.models import Q
+
+import random
+
+SEPARATOR = ':::'
+
+
+def invalidate_duplicate_ac_codes(apps, schema_editor):
+    """Invalidates the AC code by adding a prefix to the duplicate AC codes
+    that are not linked to SourceInformation objects."""
+    AcCode = apps.get_model('ocve', 'AcCode')
+    Source = apps.get_model('ocve', 'Source')
+
+    for ac in AcCode.objects.all():
+        if not Source.objects.filter(
+            sourceinformation__accode=ac).filter(Q(ocve=1) | Q(cfeo=1)):
+            ac.accode = str(random.uniform(0, 9)) + SEPARATOR + ac.accode
+            ac.save()
+
+def restore_duplicate_ac_codes(apps, schema_editor):
+    """Removes the duplicate prefix from the AC codes."""
+    AcCode = apps.get_model('ocve', 'AcCode')
+    for ac in AcCode.objects.filter(accode__contains=SEPARATOR):
+        ac.accode = ac.accode.split(SEPARATOR)[1]
+        ac.save()
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('ocve',
+         '0006_auto_20150615_1037'),
+    ]
+
+    operations = [
+        migrations.RunPython(invalidate_duplicate_ac_codes,
+                             reverse_code=restore_duplicate_ac_codes)
+    ]
