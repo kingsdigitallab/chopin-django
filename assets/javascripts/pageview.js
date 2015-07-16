4e09601605d6d9d9e499daf64835bf547c9f8d88
Ginestra <ginestra@users.noreply.github.com>
2015-07-16 14:32:17 +0100

    removing all references to resizeModal

--- assets/javascripts/pageview.js
+++ assets/javascripts/pageview.js
@@ -62,16 +62,16 @@ function updateBrowseResults() {
 
 }
 
-$(window).resize(function () {
+/* $(window).resize(function () {
     winHeight = $(window).height();
     resizeModal(winHeight);
-});
+}); */
 
 $(document).ready(function () {
 
     /* Modal fix: https://github.com/zurb/foundation/issues/2971 */
-    winHeight = $(window).height();
-    resizeModal(winHeight);
+    /* winHeight = $(window).height();
+    resizeModal(winHeight); */
 
     /* Annotations panel */
     $("a.ctrl.show-annotations").click(function () {
