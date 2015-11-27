f8137aa0b61f3561ea9a6f0eba0be021f9cc4fba
Elliott Hall <elliott.hall@kcl.ac.uk>
2015-11-27 12:39:07 +0000

    Added first version of page clone to source editor

--- ocve/dbmi/dbmi_urls.py
+++ ocve/dbmi/dbmi_urls.py
@@ -18,6 +18,7 @@ urlpatterns = patterns('', (r'^source/(?P<id>\d+)/$', source),
                        (r'^saveSource/(?P<id>\d+)/$', saveSource),
                        (r'^saveSourceInformation/(?P<id>\d+)/$', saveSourceInformation),
                        (r'^clonesource/(?P<id>\d+)/$', clonesource),
+                       (r'^clonepage/(?P<id>\d+)/$', clonepage),
                        (r'^generatesourcethumbnails/(?P<id>\d+)/$', generateSourceThumbnails),
 
                        #Source/work components
