3d3473dc2872ba790452e1dfffce868bc05d1d93
Elliott Hall <elliott.hall@kcl.ac.uk>
2015-12-03 15:16:40 +0000

    Initial setup of mei code.

---
+++ ocve/dbmi/meiviews.py
@@ -0,0 +1,20 @@
+__author__ = 'elliotthall'
+from django.shortcuts import render_to_response
+from django.template.context import RequestContext
+from ocve.models import *
+
+#Populate the meieditor for editing
+    #Non-prototype will have extra navigation objects
+def meieidtor(request,id=65054):
+    pi=PageImage.objects.get(id=id)
+    return render_to_response('dbmi/meieditor.html', {'pi': pi}, context_instance=RequestContext(request))
+
+def savemei(request,id):
+    try:
+        mei=request.POST['mei']
+        pi=PageImage.objects.get(id=id)
+        pi.mei=mei
+        pi.save()
+    except Exception, e:
+        print e
+    return meieidtor(request,id)
