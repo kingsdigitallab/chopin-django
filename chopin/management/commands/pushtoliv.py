2d7c0aff8d675a7d8f192321079dfae9a7c6acd1
Elliott Hall <elliott.hall@kcl.ac.uk>
2016-02-04 18:01:44 +0000

    Added revert option

--- chopin/management/commands/pushtoliv.py
+++ chopin/management/commands/pushtoliv.py
@@ -1,5 +1,5 @@
 __author__ = 'elliotthall'
-from django.core.management.base import NoArgsCommand
+from django.core.management.base import BaseCommand,NoArgsCommand
 import logging
 import subprocess
 
@@ -7,50 +7,73 @@ logging.basicConfig(format='%(asctime)-15s %(message)s')
 logger = logging.getLogger('Main logger')
 
 
-class Command(NoArgsCommand):
-    help = """Dumps data from stg and pushes to live, along with static JSON. NOTE: Works only on VM"""
+class Command(BaseCommand):
+    help = """Dumps data from stg and pushes to live, along with static JSON.  NOTE: Works only on VM"""
+
+    def add_arguments(self, parser):
+        parser.add_argument('--revert',
+            action='store_true',
+            dest='revert',
+            default=False,
+            help='Restore from last liv dump')
+
 
     def cpscript(self,script,source,dest):
         cpcmd=['cp',source+script,dest+script]
         subprocess.Popen(cpcmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-    def handle_noargs(self, **options):
+    def handle_noargs(self, *args, **options):
         stg_db='ocve2real'
         liv_db='chopin_liv'
         stg_scripts='/vol/ocve3/webroot/stg/django/chopin/static/javascripts/'
         liv_scripts='/vol/ocve3/webroot/liv/django/chopin/static/javascripts/'
         dump_scripts='/vol/ocve3/dumps/javascript/'
         #' | ','mysql','-u root',liv_db,' < ',' mydb2' shell=True
-        pushstat = ['mysqldump','-u root',stg_db,'>',' /vol/ocve3/dumps/stg_dump.sql']
-        proc = subprocess.Popen(pushstat, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        err = proc.communicate()[1]
-        if err:
-            logger.error('Dump from stg failed with error:'+str(err))
-            return False
+        if options['revert']:
+            #Revert scripts
+            self.cpscript('OCVEsourceJSON.js',dump_scripts,liv_scripts)
+            self.cpscript('CFEOsourceJSON.js',dump_scripts,liv_scripts)
+            revertstat = ['mysql','-u root',liv_db,'<',' /vol/ocve3/dumps/liv_dump.sql']
+            proc = subprocess.Popen(revertstat, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            err = proc.communicate()[1]
+            if err:
+                logger.error('Dump from stg failed with error:'+str(err))
+                return False
+            else:
+                logger.info('stg dump generated')
+
         else:
-            logger.info('stg dump generated')
-            #backup live
             pushstat = ['mysqldump','-u root',stg_db,'>',' /vol/ocve3/dumps/stg_dump.sql']
             proc = subprocess.Popen(pushstat, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             err = proc.communicate()[1]
             if err:
-                logger.error('Dump from liv failed with error:'+str(err))
+                logger.error('Dump from stg failed with error:'+str(err))
                 return False
             else:
-                #Backup live JSON
-                self.cpscript('OCVEsourceJSON.js',liv_scripts,dump_scripts)
-                self.cpscript('CFEOsourceJSON.js',liv_scripts,dump_scripts)
-                #Copy rebuilt stg JSON to live.
-                self.cpscript('OCVEsourceJSON.js',stg_scripts,liv_scripts)
-                self.cpscript('CFEOsourceJSON.js',stg_scripts,liv_scripts)
-                #Push stg mysql to
-                # pushstat = ['mysqldump','-u root',stg_db,'>',' /vol/ocve3/dumps/stg_dump.sql']
-                # proc = subprocess.Popen(pushstat, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                # err = proc.communicate()[1]
-                # if err:
-                #     logger.error('Push to live failed with error:'+str(err))
-                #     return False
-                # else:
-                #     logger.info('stg data pushed to live')
-                #TODO add touch?
+                logger.info('stg dump generated')
+                #backup live
+                pushstat = ['mysqldump','-u root',stg_db,'>',' /vol/ocve3/dumps/liv_dump.sql']
+                proc = subprocess.Popen(pushstat, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                err = proc.communicate()[1]
+                if err:
+                    logger.error('Dump from liv failed with error:'+str(err))
+                    return False
+                else:
+                    #Backup live JSON
+                    self.cpscript('OCVEsourceJSON.js',liv_scripts,dump_scripts)
+                    self.cpscript('CFEOsourceJSON.js',liv_scripts,dump_scripts)
+                    #Copy rebuilt stg JSON to live.
+                    self.cpscript('OCVEsourceJSON.js',stg_scripts,liv_scripts)
+                    self.cpscript('CFEOsourceJSON.js',stg_scripts,liv_scripts)
+                    #Push stg mysql to
+                    # pushstat = ['mysqldump','-u root',stg_db,'>',' /vol/ocve3/dumps/stg_dump.sql']
+                    # proc = subprocess.Popen(pushstat, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                    # err = proc.communicate()[1]
+                    # if err:
+                    #     logger.error('Push to live failed with error:'+str(err))
+                    #     return False
+                    # else:
+                    #     logger.info('stg data pushed to live')
+                    #TODO add touch?
+
 
