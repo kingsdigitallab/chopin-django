5bbc96bad80ee4940e871711f83b1269b9a7d986
ehall <elliott.hall@kcl.ac.uk>
2016-02-15 17:24:35 +0000

    Initial branch template for ol3, require-js added

---
+++ assets/javascripts/pageview-main.js
@@ -0,0 +1,30 @@
+/**
+ * Created by elliotthall on 15/02/16.
+ */
+
+requirejs.config({
+    //By default load any module IDs from js/lib
+    //baseUrl: 'js/lib',
+    //except, if the module ID starts with "app",
+    //load it from the js/app directory. paths
+    //config is relative to the baseUrl, and
+    //never includes a ".js" extension since
+    //the paths config could be for a directory.
+    paths: {
+        vendor: '../vendor',
+        ol3: '../vendor/ol v3.13.1',
+        jquery: '../vendor/jquery/dist/jquery'
+    }
+});
+
+requirejs(["jquery","ol3/build/ol","pageview-ol3"], function($,ol,pageview) {
+    //This function is called when scripts/helper/util.js is loaded.
+    //If util.js calls define(), then this function is not fired until
+    //util's dependencies have loaded, and the util argument will hold
+    //the module value for "helper/util".
+    $(document).ready(function () {
+       pageview.initMap(ol);
+    });
+
+});
+
