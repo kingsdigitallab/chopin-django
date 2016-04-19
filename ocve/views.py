7d87d90a4c97ea5bc41db8ecf04d1a00cce48944
ehall <ehall@localhost>
2016-04-19 14:33:47 +0100

    login page fixes

--- ocve/views.py
+++ ocve/views.py
@@ -54,7 +54,7 @@ def updateSourceComponent(request):
 
 
 def login_page(request):
-    return render_to_response('registration/login-page.html')
+    return render_to_response('registration/login-page.html',{},context_instance=RequestContext(request))
 
 
 def user_profile(request):
