edf8fbd6e8d66b2553b8651c22097df16cc1d258
Elliott Hall <elliott.hall@kcl.ac.uk>
2016-08-17 11:54:23 +0100

    Removed debug toolbar due to possible django version conflict

--- chopin/settings/dev.py
+++ chopin/settings/dev.py
@@ -45,15 +45,15 @@ except ImportError:
 # http://django-debug-toolbar.readthedocs.org/en/latest/
 # -----------------------------------------------------------------------------
 
-try:
-    import debug_toolbar  # noqa
-
-    INSTALLED_APPS = INSTALLED_APPS + ('debug_toolbar',)
-    MIDDLEWARE_CLASSES = MIDDLEWARE_CLASSES + (
-        'debug_toolbar.middleware.DebugToolbarMiddleware',)
-    DEBUG_TOOLBAR_PATCH_SETTINGS = True
-except ImportError:
-    pass
+# try:
+#     import debug_toolbar  # noqa
+#
+#     INSTALLED_APPS = INSTALLED_APPS + ('debug_toolbar',)
+#     MIDDLEWARE_CLASSES = MIDDLEWARE_CLASSES + (
+#         'debug_toolbar.middleware.DebugToolbarMiddleware',)
+#     DEBUG_TOOLBAR_PATCH_SETTINGS = True
+# except ImportError:
+#     pass
 
 # -----------------------------------------------------------------------------
 # Local settings
