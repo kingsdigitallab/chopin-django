1ff3c6a8e90aa76d8554f61c1883b825b031356a
Elliott Hall <elliott.hall@kcl.ac.uk>
2016-09-01 11:44:54 +0100

    Added a radio button and extra form validation to uploader

---
+++ chopin/settings/stg.py
@@ -0,0 +1,28 @@
+from base import *  # noqa
+
+CACHE_REDIS_DATABASE = '1'
+CACHES['default']['LOCATION'] = '127.0.0.1:6379:' + CACHE_REDIS_DATABASE
+
+INTERNAL_IPS = ('ocve3-stg.dighum.kcl.ac.uk',)
+ALLOWED_HOSTS = ['ocve3-stg.dighum.kcl.ac.uk', 'www.chopinonline.ac.uk']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'app_ocve_merged_stg',
+        'USER': 'app_ocve',
+        'PASSWORD': '',
+        'HOST': 'db-pg-1.cch.kcl.ac.uk'
+    }
+}
+
+HAYSTACK_CONNECTIONS['default']['INDEX_NAME'] = PROJECT_NAME + '_stg'
+
+# -----------------------------------------------------------------------------
+# Local settings
+# -----------------------------------------------------------------------------
+
+try:
+    from local import *  # noqa
+except ImportError:
+    pass
