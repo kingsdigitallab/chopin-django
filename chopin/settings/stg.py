72ab7a26819d0cbc5f69506ec71fd9971ef33ab1
Elliott Hall <elliott.hall@kcl.ac.uk>
2016-11-01 16:48:56 +0000

    pushtoliv bash added

---
+++ chopin/settings/stg.py
@@ -0,0 +1,39 @@
+from base import *  # noqa
+
+CACHE_REDIS_DATABASE = '1'
+CACHES['default']['LOCATION'] = '127.0.0.1:6379:' + CACHE_REDIS_DATABASE
+
+INTERNAL_IPS = ('ocve3-stg.dighum.kcl.ac.uk',)
+ALLOWED_HOSTS = ['ocve3-stg.dighum.kcl.ac.uk', 'www.chopinonline.ac.uk']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'app_ocve_merged_stg',
+        'USER': 'app_ocve',
+        'PASSWORD': '',
+        'HOST': 'db-pg-1.cch.kcl.ac.uk'
+    }
+}
+
+HAYSTACK_CONNECTIONS['default']['INDEX_NAME'] = PROJECT_NAME + '_stg'
+
+CELERYBEAT_SCHEDULE = {
+    'haystack-update-index-every-day': {
+        'task': 'catalogue.tasks.haystack_update_index',
+        'schedule': crontab(minute=0, hour=2),
+    },
+    'push-to-live-every-day': {
+        'task': 'ocve.tasks.push_to_liv',
+        'schedule': crontab(minute=0, hour=1),
+    },
+}
+
+# -----------------------------------------------------------------------------
+# Local settings
+# -----------------------------------------------------------------------------
+
+try:
+    from local import *  # noqa
+except ImportError:
+    pass
