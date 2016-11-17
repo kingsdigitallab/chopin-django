513002a41567896773ae0f79fb9c50d4ad1bae16
ehall <elliott.hall@kcl.ac.uk>
2016-11-17 11:49:52 +0000

    Translate function added, finishdraw refactored

--- assets/javascripts/pageview-main.js
+++ assets/javascripts/pageview-main.js
@@ -3,13 +3,6 @@
  */
 
 requirejs.config({
-    //By default load any module IDs from js/lib
-    //baseUrl: 'js/lib',
-    //except, if the module ID starts with "app",
-    //load it from the js/app directory. paths
-    //config is relative to the baseUrl, and
-    //never includes a ".js" extension since
-    //the paths config could be for a directory.
     paths: {
         vendor: '../vendor',
         ol3: '../vendor/OpenLayers/3.18.2/ol',
@@ -22,10 +15,7 @@ requirejs.config({
 });
 
 requirejs(["jquery","ol3","pageview-ol3"], function($,ol,pageview) {
-    //This function is called when scripts/helper/util.js is loaded.
-    //If util.js calls define(), then this function is not fired until
-    //util's dependencies have loaded, and the util argument will hold
-    //the module value for "helper/util".
+
     'use strict';
 
   $(document).ready(function() {
