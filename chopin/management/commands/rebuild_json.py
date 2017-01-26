8bf3255cf124e6b3547a9ecd317fc479707d4a58
ehall <elliott.hall@kcl.ac.uk>
2017-01-26 11:31:22 +0000

    Added rebuil_json,build_missing_thumbnail commands

---
+++ chopin/management/commands/rebuild_json.py
@@ -0,0 +1,21 @@
+#Rebuild the Json data for the web front end
+__author__ = 'elliotthall'
+from django.core.management.base import BaseCommand, NoArgsCommand
+import logging
+import subprocess
+from optparse import make_option
+from ocve.uitools import serializeOCVESourceJson,serializeCFEOSourceJson,serializeAcCodeConnector
+
+logger = logging.getLogger(__name__)
+
+class Command(BaseCommand):
+    help = """Rebuild website JSON"""
+
+    def handle(self, *args, **options):
+        logger.info('rebuild OCVE JSON')
+        serializeOCVESourceJson()
+        logger.info('rebuild CFEO JSON')
+        serializeCFEOSourceJson()
+        logger.info('rebuild AC JSON')
+        serializeAcCodeConnector()
+
