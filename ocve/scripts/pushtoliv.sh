d3b6cb70a928d1974af5b2d9ecef120feccc6aa0
Elliott Hall <elliott.hall@kcl.ac.uk>
2017-01-26 11:31:22 +0000

    Added rebuil_json,build_missing_thumbnail commands

--- ocve/scripts/pushtoliv.sh
+++ ocve/scripts/pushtoliv.sh
@@ -7,20 +7,66 @@ dump_dir="/vol/ocve3/dumps/"
 log_file="${dump_dir}/pushtoliv.log"
 stg_dump="${dump_dir}pg_stg_dump.sql"
 live_dump="${dump_dir}pg_liv_dump.sql"
+notes_dump="${dump_dir}notes_liv_dump.sql"
 
 printf "Pushtolive begin $(date +%F_%T) \n" >> ${log_file}
-#Backup staging
-pg_dump -w -c -O -h db-pg-1.cch.kcl.ac.uk -U app_ocve -t ocve_* -T ocve_Annotation*  app_ocve_merged_stg > ${stg_dump} 2>>${log_file}
+
 #Backup Live
 pg_dump -w -c -O -h db-pg-1.cch.kcl.ac.uk -U app_ocve app_ocve_merged > ${live_dump} 2>>${log_file}
+
+#Backup staging
+pg_dump -w -c -O -h db-pg-1.cch.kcl.ac.uk -U app_ocve -t ocve_* -T ocve_Annotation*  app_ocve_merged_stg > ${stg_dump} 2>>${log_file}
+
 #Upload staging to live if file ok
 if [ -s $stg_dump ]; then
     #Add line to log
     printf "$(date +%F_%T): Uploading to live \n" >> ${log_file}
+    #Backup notes from live
+    pg_dump -w -a -h db-pg-1.cch.kcl.ac.uk --table=ocve_annotation_barbaregion --table=ocve_annotation -U app_ocve app_ocve_merged > ${notes_dump} 2>>${log_file}
+
+    #delete notes set on live
+    psql app_ocve_merged -c "delete from ocve_annotation_barregion;"
+    psql app_ocve_merged -c "delete from ocve_annotation;"
+
     #upload to live
     psql -w -h db-pg-1.cch.kcl.ac.uk -U app_ocve app_ocve_merged < ${stg_dump} 2>>${log_file}
+
+    #restore notes to live
+    psql -w -h db-pg-1.cch.kcl.ac.uk -U app_ocve app_ocve_merged < ${notes_dump} 2>>${log_file}
+
+    #Rebuild website data on liv
+
+    #Create missing thumbnails
     
 else
     printf "ERROR: staging dump not generated.  Upload aborted \n"
 fi
 printf "$(date +%F_%T): pushtolive complete \n" >> ${log_file}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
