17c2f4255f4ea150f935e75de3575ab07c928205
Elliott Hall <elliott.hall@kcl.ac.uk>
2017-01-26 11:31:22 +0000

    Added rebuil_json,build_missing_thumbnail commands

---
+++ chopin/management/commands/build_missing_thumbnails.py
@@ -0,0 +1,26 @@
+#Rebuild the Json data for the web front end
+__author__ = 'elliotthall'
+from django.core.management.base import BaseCommand, NoArgsCommand
+import logging
+import subprocess
+from optparse import make_option
+
+from ocve.models import PageImage
+from django.db.models import Q
+import os
+
+logger = logging.getLogger(__name__)
+
+class Command(BaseCommand):
+    help = """Look for missing thumbnails and add them.  DOES NOT REWRITE EXISTING THUMBNAILS"""
+
+    def handle(self, *args, **options):
+        #Get all pageimages that are to be shown (in stg or live)
+        pages=PageImage.objects.filter(Q(page__sourcecomponent__source__ocve=1) | Q(page__sourcecomponent__source__cfeo=1))
+        #Check for file
+        for pageimage in pages:
+            fname = os.path.join(settings.THUMBNAIL_DIR, str(pageimage.id) + ".jpg")
+            if not os.path.isfile(fname):
+                #Missing, write
+                log.info('Writing thumbnail '+fname)
+                generateThumbnail(pageimage)
