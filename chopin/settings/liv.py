aad2e7dc4c58bc1b8ee628971de48079f8794f64
Elliott Hall <elliott.hall@kcl.ac.uk>
2017-02-07 17:19:34 +0000

    Merge

---
+++ chopin/settings/liv.py
@@ -0,0 +1,32 @@
+from base import *  # noqa
+
+CACHE_REDIS_DATABASE = '0'
+CACHES['default']['LOCATION'] = '127.0.0.1:6379:' + CACHE_REDIS_DATABASE
+CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379:' + CACHE_REDIS_DATABASE
+BROKER_URL = 'redis://127.0.0.1:6379:' + CACHE_REDIS_DATABASE
+
+INTERNAL_IPS = ('ocve3.dighum.kcl.ac.uk', )
+ALLOWED_HOSTS = ['ocve3.dighum.kcl.ac.uk', 'www.chopinonline.ac.uk',
+                 'chopinonline.ac.uk']
+
+# Build JSON with the live flagged sources only
+BUILD_LIVE_ONLY = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'app_ocve_merged_test',
+        'USER': 'app_ocve',
+        'PASSWORD': '',
+        'HOST': 'db-pg-1.cch.kcl.ac.uk'
+    }
+}
+
+# -----------------------------------------------------------------------------
+# Local settings
+# -----------------------------------------------------------------------------
+
+try:
+    from local import *  # noqa
+except ImportError:
+    pass
---
+++ ocve/tests/test_ui_views.py
@@ -0,0 +1,24 @@
+__author__ = 'elliotthall'
+
+
+from django.test import TestCase
+from django.test import Client
+
+from ocve.models import *
+from ocve.uitools import *
+from ocve.uiviews import *
+
+class TestUIViews(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_ocvePageImageView(self):
+        pass
+
+    def test_ocveBarView(self):
+        pass
+
+    #CFEO Views
+    def test_cfeoPageImageView(self):
+        pass
