988c6a5b401840c25535b0ce8d9129d14de5bf8f
Elliott Hall <elliott.hall@kcl.ac.uk>
2017-02-08 12:46:40 +0000

    padding increased for font sizes

---
+++ ocve/scripts/migrate_mysql/truncate.sql
@@ -0,0 +1,266 @@
+-- --TRUNCATE ocve_accode;
+-- --TRUNCATE "ocve_accode" CASCADE;
+
+----SELECT pg_catalog.setval(pg_get_serial_sequence('ocve_accode', 'id'), 3764, true);
+
+-- --TRUNCATE ocve_annotation;
+--TRUNCATE "ocve_annotation" CASCADE;
+
+----SELECT pg_catalog.setval(pg_get_serial_sequence('ocve_annotation', 'id'), 5869, true);
+
+-- --TRUNCATE ocve_annotation_barregion;
+--TRUNCATE "ocve_annotation_barregion" CASCADE;
+
+--SELECT pg_catalog.setval(pg_get_serial_sequence('ocve_annotation_barregion', 'id'), 6436, true);
+
+-- --TRUNCATE ocve_annotationtype;
+--TRUNCATE "ocve_annotationtype" CASCADE;
+
+--SELECT pg_catalog.setval(pg_get_serial_sequence('ocve_annotationtype', 'id'), 5, true);
+
+-- --TRUNCATE ocve_archive;
+--TRUNCATE "ocve_archive" CASCADE;
+
+--SELECT pg_catalog.setval(pg_get_serial_sequence('ocve_archive', 'id'), 1860, true);
+
+-- --TRUNCATE ocve_bar;
+--TRUNCATE "ocve_bar" CASCADE;
+
+--SELECT pg_catalog.setval(pg_get_serial_sequence('ocve_bar', 'id'), 28654, true);
+
+-- --TRUNCATE ocve_bar_barregion;
+--TRUNCATE "ocve_bar_barregion" CASCADE;
+
+--SELECT pg_catalog.setval(pg_get_serial_sequence('ocve_bar_barregion', 'id'), 522445, true);
+
+-- --TRUNCATE ocve_barcollection;
+--TRUNCATE "ocve_barcollection" CASCADE;
+
+--SELECT pg_catalog.setval(pg_get_serial_sequence('ocve_barcollection', 'id'), 9, true);
+
+-- --TRUNCATE ocve_barcollection_regions;
+--TRUNCATE "ocve_barcollection_regions" CASCADE;
+
+--SELECT pg_catalog.setval(pg_get_serial_sequence('ocve_barcollection_regions', 'id'), 32, true);
+
+-- --TRUNCATE ocve_barregion;
+--TRUNCATE "ocve_barregion" CASCADE;
+
+--SELECT pg_catalog.setval(pg_get_serial_sequence('ocve_barregion', 'id'), 530783, true);
+
+-- --TRUNCATE ocve_barsequence;
+--TRUNCATE "ocve_barsequence" CASCADE;
+
+--SELECT pg_catalog.setval(pg_get_serial_sequence('ocve_barsequence', 'id'), 16706, true);
+
+-- --TRUNCATE ocve_barspine;
+--TRUNCATE "ocve_barspine" CASCADE;
+
+--SELECT pg_catalog.setval(pg_get_serial_sequence('ocve_barspine', 'id'), 996879, true);
+
+-- --TRUNCATE ocve_city;
+--TRUNCATE "ocve_city" CASCADE;
+
+--SELECT pg_catalog.setval(pg_get_serial_sequence('ocve_city', 'id'), 1779, true);
+
+-- --TRUNCATE ocve_collectiontype;
+--TRUNCATE "ocve_collectiontype" CASCADE;
+
+--SELECT pg_catalog.setval(pg_get_serial_sequence('ocve_collectiontype', 'id'), 4, true);
+
+-- --TRUNCATE ocve_country;
+--TRUNCATE "ocve_country" CASCADE;
+
+--SELECT pg_catalog.setval(pg_get_serial_sequence('ocve_country', 'id'), 735, true);
+
+-- --TRUNCATE ocve_dedicatee;
+--TRUNCATE "ocve_dedicatee" CASCADE;
+
+--SELECT pg_catalog.setval(pg_get_serial_sequence('ocve_dedicatee', 'id'), 1521, true);
+
+-- --TRUNCATE ocve_editstatus;
+--TRUNCATE "ocve_editstatus" CASCADE;
+
+--SELECT pg_catalog.setval(pg_get_serial_sequence('ocve_editstatus', 'id'), 13, true);
+
+-- --TRUNCATE ocve_genre;
+--TRUNCATE "ocve_genre" CASCADE;
+
+--SELECT pg_catalog.setval(pg_get_serial_sequence('ocve_genre', 'id'), 2145, true);
+
+-- --TRUNCATE ocve_genre_work;
+--TRUNCATE "ocve_genre_work" CASCADE;
+
+--SELECT pg_catalog.setval(pg_get_serial_sequence('ocve_genre_work', 'id'), 4900, true);
+
+-- --TRUNCATE ocve_instrument;
+--TRUNCATE "ocve_instrument" CASCADE;
+
+--SELECT pg_catalog.setval(pg_get_serial_sequence('ocve_instrument', 'id'), 351, true);
+
+-- --TRUNCATE ocve_instrumentcomponent;
+--TRUNCATE "ocve_instrumentcomponent" CASCADE;
+
+--SELECT pg_catalog.setval(pg_get_serial_sequence('ocve_instrumentcomponent', 'id'), 690, true);
+
+-- --TRUNCATE ocve_keymode;
+--TRUNCATE "ocve_keymode" CASCADE;
+
+--SELECT pg_catalog.setval(pg_get_serial_sequence('ocve_keymode', 'id'), 4, true);
+
+-- --TRUNCATE ocve_keypitch;
+--TRUNCATE "ocve_keypitch" CASCADE;
+
+--SELECT pg_catalog.setval(pg_get_serial_sequence('ocve_keypitch', 'id'), 1528, true);
+
+-- --TRUNCATE ocve_newpageimage;
+--TRUNCATE "ocve_newpageimage" CASCADE;
+
+--SELECT pg_catalog.setval(pg_get_serial_sequence('ocve_newpageimage', 'id'), 8792, true);
+
+-- --TRUNCATE ocve_newsource;
+--TRUNCATE "ocve_newsource" CASCADE;
+
+--SELECT pg_catalog.setval(pg_get_serial_sequence('ocve_newsource', 'id'), 626, true);
+
+-- --TRUNCATE ocve_newsourceinformation;
+--TRUNCATE "ocve_newsourceinformation" CASCADE;
+
+--SELECT pg_catalog.setval(pg_get_serial_sequence('ocve_newsourceinformation', 'id'), 1, true);
+
+-- --TRUNCATE ocve_ocveuser;
+--TRUNCATE "ocve_ocveuser" CASCADE;
+
+-- --TRUNCATE ocve_opus;
+--TRUNCATE "ocve_opus" CASCADE;
+
+--SELECT pg_catalog.setval(pg_get_serial_sequence('ocve_opus', 'id'), 6684, true);
+
+-- --TRUNCATE ocve_page;
+--TRUNCATE "ocve_page" CASCADE;
+
+--SELECT pg_catalog.setval(pg_get_serial_sequence('ocve_page', 'id'), 76856, true);
+
+-- --TRUNCATE ocve_pageimage;
+--TRUNCATE "ocve_pageimage" CASCADE;
+
+--SELECT pg_catalog.setval(pg_get_serial_sequence('ocve_pageimage', 'id'), 76842, true);
+
+-- --TRUNCATE ocve_pagelegacy;
+--TRUNCATE "ocve_pagelegacy" CASCADE;
+
+--SELECT pg_catalog.setval(pg_get_serial_sequence('ocve_pagelegacy', 'id'), 76840, true);
+
+-- --TRUNCATE ocve_pagetype;
+--TRUNCATE "ocve_pagetype" CASCADE;
+
+--SELECT pg_catalog.setval(pg_get_serial_sequence('ocve_pagetype', 'id'), 9, true);
+
+-- --TRUNCATE ocve_printingmethod;
+--TRUNCATE "ocve_printingmethod" CASCADE;
+
+--SELECT pg_catalog.setval(pg_get_serial_sequence('ocve_printingmethod', 'id'), 77, true);
+
+-- --TRUNCATE ocve_publisher;
+--TRUNCATE "ocve_publisher" CASCADE;
+
+--SELECT pg_catalog.setval(pg_get_serial_sequence('ocve_publisher', 'id'), 2378, true);
+
+-- --TRUNCATE ocve_source;
+--TRUNCATE "ocve_source" CASCADE;
+
+--SELECT pg_catalog.setval(pg_get_serial_sequence('ocve_source', 'id'), 18843, true);
+
+-- --TRUNCATE ocve_sourcecomponent;
+--TRUNCATE "ocve_sourcecomponent" CASCADE;
+
+--SELECT pg_catalog.setval(pg_get_serial_sequence('ocve_sourcecomponent', 'id'), 42597, true);
+
+-- --TRUNCATE ocve_sourcecomponent_instrument;
+--TRUNCATE "ocve_sourcecomponent_instrument" CASCADE;
+
+--SELECT pg_catalog.setval(pg_get_serial_sequence('ocve_sourcecomponent_instrument', 'id'), 7347, true);
+
+-- --TRUNCATE ocve_sourcecomponent_workcomponent;
+--TRUNCATE "ocve_sourcecomponent_workcomponent" CASCADE;
+
+--SELECT pg_catalog.setval(pg_get_serial_sequence('ocve_sourcecomponent_workcomponent', 'id'), 35079, true);
+
+-- --TRUNCATE ocve_sourcecomponenttype;
+--TRUNCATE "ocve_sourcecomponenttype" CASCADE;
+
+--SELECT pg_catalog.setval(pg_get_serial_sequence('ocve_sourcecomponenttype', 'id'), 1474, true);
+
+-- --TRUNCATE ocve_sourceinformation;
+--TRUNCATE "ocve_sourceinformation" CASCADE;
+
+--SELECT pg_catalog.setval(pg_get_serial_sequence('ocve_sourceinformation', 'id'), 18398, true);
+
+-- --TRUNCATE ocve_sourceinformation_printingmethod;
+--TRUNCATE "ocve_sourceinformation_printingmethod" CASCADE;
+
+--SELECT pg_catalog.setval(pg_get_serial_sequence('ocve_sourceinformation_printingmethod', 'id'), 4007, true);
+
+-- --TRUNCATE ocve_sourceinformation_year;
+--TRUNCATE "ocve_sourceinformation_year" CASCADE;
+
+----SELECT pg_catalog.setval(pg_get_serial_sequence('ocve_sourceinformation_year', 'id'), 463, true);
+
+-- --TRUNCATE ocve_sourcelegacy;
+--TRUNCATE "ocve_sourcelegacy" CASCADE;
+
+----SELECT pg_catalog.setval(pg_get_serial_sequence('ocve_sourcelegacy', 'id'), 5206, true);
+
+-- --TRUNCATE ocve_sourcetype;
+--TRUNCATE "ocve_sourcetype" CASCADE;
+
+----SELECT pg_catalog.setval(pg_get_serial_sequence('ocve_sourcetype', 'id'), 4, true);
+
+-- --TRUNCATE ocve_tonekey;
+--TRUNCATE "ocve_tonekey" CASCADE;
+
+----SELECT pg_catalog.setval(pg_get_serial_sequence('ocve_tonekey', 'id'), 33, true);
+
+-- --TRUNCATE ocve_tonenote;
+--TRUNCATE "ocve_tonenote" CASCADE;
+
+----SELECT pg_catalog.setval(pg_get_serial_sequence('ocve_tonenote', 'id'), 14, true);
+
+
+----SELECT pg_catalog.setval(pg_get_serial_sequence('ocve_treetype', 'id'), 9, true);
+
+-- --TRUNCATE ocve_uncertain;
+--TRUNCATE "ocve_uncertain" CASCADE;
+
+----SELECT pg_catalog.setval(pg_get_serial_sequence('ocve_uncertain', 'id'), 1, true);
+
+--TRUNCATE ocve_work;
+TRUNCATE "ocve_work" CASCADE;
+
+SELECT pg_catalog.setval(pg_get_serial_sequence('ocve_work', 'id'), 6428, true);
+
+-- --TRUNCATE ocve_workcollection;
+TRUNCATE "ocve_workcollection" CASCADE;
+
+SELECT pg_catalog.setval(pg_get_serial_sequence('ocve_workcollection', 'id'), 6, true);
+
+-- --TRUNCATE ocve_workcomponent;
+--TRUNCATE "ocve_workcomponent" CASCADE;
+
+----SELECT pg_catalog.setval(pg_get_serial_sequence('ocve_workcomponent', 'id'), 9588, true);
+
+-- --TRUNCATE ocve_workcomponenttype;
+--TRUNCATE "ocve_workcomponenttype" CASCADE;
+
+----SELECT pg_catalog.setval(pg_get_serial_sequence('ocve_workcomponenttype', 'id'), 1, true);
+
+-- --TRUNCATE ocve_workinformation;
+TRUNCATE "ocve_workinformation" CASCADE;
+
+SELECT pg_catalog.setval(pg_get_serial_sequence('ocve_workinformation', 'id'), 6424, true);
+
+-- --TRUNCATE ocve_year;
+--TRUNCATE "ocve_year" CASCADE;
+
+----SELECT pg_catalog.setval(pg_get_serial_sequence('ocve_year', 'id'), 55, true);
+
